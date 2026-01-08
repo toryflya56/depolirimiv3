16964a3b577b7977c3d8247a5c3481a4f96d44d1
toryflya56 <toryflya56@gmail.com>
2026-01-08 09:51:39 +0000

    refactor: remove sections from homepage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,10 +1,5 @@
-import React, { Suspense } from 'react';
+import React from 'react';
 import HeroLanding from '../features/studio/components/HeroLanding';
-
-const OurExpertise = React.lazy(() => import('../features/studio/components/OurExpertise'));
-const ServicesSection = React.lazy(() => import('../features/catalog/components/ServicesSection').then(module => ({ default: module.ServicesSection })));
-const Testimonials = React.lazy(() => import('../features/studio/components/Testimonials').then(module => ({ default: module.Testimonials })));
-const ContactSection = React.lazy(() => import('../features/studio/components/ContactSection').then(module => ({ default: module.ContactSection })));
 
 export const HomePage: React.FC = () => {
   return (
@@ -14,12 +9,6 @@ export const HomePage: React.FC = () => {
       </a>
       <main id="main-content">
         <HeroLanding />
-        <Suspense fallback={<div className="bg-black text-white text-center py-12">Loading...</div>}>
-          <OurExpertise />
-          <ServicesSection />
-          <Testimonials />
-          <ContactSection />
-        </Suspense>
       </main>
     </div>
   );
